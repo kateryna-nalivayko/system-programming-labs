5229eb1f256e793f38fcd58f2080d1263c7ffbae
kateryna-nalivayko <katenalivayko@knu.ua>
2025-10-09 12:34:35 +0300

    refactor worley_parser.py, know that is class EarleyParser

--- lab_3_syntax_analysis/src/parser/worley_parser.py
+++ lab_3_syntax_analysis/src/parser/worley_parser.py
@@ -1,80 +1,105 @@
-terminals = ['+', '1']
-grammar = [['s', ['e']],
-           ['e', ['1']],
-           ['e', ['e', '+', 'e']]]
+from dataclasses import dataclass
+from typing import Iterable, List, Any
+
+DEFAULT_TERMINALS = ['+', '1']
+DEFAULT_GRAMMAR = [
+    ['s', ['e']],
+    ['e', ['1']],
+    ['e', ['e', '+', 'e']],
+]
 
 
+@dataclass(eq=True, frozen=True)
 class Task:
-    def __init__(self, rule, dot, start):
-        self.rule = rule
-        self.dot = dot
-        self.start = start
+    rule: int
+    dot: int
+    start: int
 
-    def next_symbol(self):
-        prod = grammar[self.rule][1]
-        return prod[self.dot] if self.dot < len(prod) else None
 
-    def __eq__(self, other):
-        return self.rule == other.rule and self.dot == other.dot and self.start == other.start
+class EarleyParser:
+    def __init__(self, terminals=None, grammar=None, debug: bool = False):
+        self.terminals = set(terminals or DEFAULT_TERMINALS)
+        self.grammar = grammar or DEFAULT_GRAMMAR
+        self.debug = debug
+        self.chart: List[List[Task]] = []
+        self.tokens: List[Any] = []
 
-    def __repr__(self):
-        prod = grammar[self.rule][1]
-        a = ' '.join(prod[:self.dot])
-        b = ' '.join(prod[self.dot:])
+    def _rhs(self, rule_idx: int) -> List[str]:
+        return self.grammar[rule_idx][1]
+
+    def _lhs(self, rule_idx: int) -> str:
+        return self.grammar[rule_idx][0]
+
+    def _add_task(self, i: int, task: Task) -> None:
+        while len(self.chart) <= i:
+            self.chart.append([])
+        if task not in self.chart[i]:
+            self.chart[i].append(task)
+
+    def recognize(self, tokens: Iterable[Any]) -> bool:
+        self.chart = [[Task(0, 0, 0)]]
+        self.tokens = list(tokens)
+
+        pos = 0
+        while True:
+            token = self.tokens[pos] if pos < len(self.tokens) else None
+
+            if pos == len(self.chart):
+                return False
+
+            i = 0
+            while i < len(self.chart[pos]):
+                task = self.chart[pos][i]
+                rhs = self._rhs(task.rule)
+                next_sym = rhs[task.dot] if task.dot < len(rhs) else None
+
+                if next_sym is None:
+                    # Completion: A→α.
+                    A = self._lhs(task.rule)
+                    for prev in self.chart[task.start]:
+                        prev_rhs = self._rhs(prev.rule)
+                        prev_next = prev_rhs[prev.dot] if prev.dot < len(prev_rhs) else None
+                        if prev_next == A:
+                            self._add_task(pos, Task(prev.rule, prev.dot + 1, prev.start))
+
+                elif next_sym in self.terminals:
+                    # Scanning
+                    if token is not None and token == next_sym:
+                        self._add_task(pos + 1, Task(task.rule, task.dot + 1, task.start))
+
+                else:
+                    # Prediction
+                    for idx, (lhs, rhs2) in enumerate(self.grammar):
+                        if lhs == next_sym:
+                            self._add_task(pos, Task(idx, 0, pos))
+
+                i += 1
+
+            if token is None:
+                break
+            pos += 1
+
+        goal_len = len(self._rhs(0))
+        goal = any(t.rule == 0 and t.dot == goal_len and t.start == 0 for t in self.chart[-1])
+
+        if self.debug:
+            self.pretty_print_chart()
+        return goal
+
+    def pretty_print_chart(self) -> None:
         week = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
-        return f'({grammar[self.rule][0]}->{a}.{b}, {week[self.start]})'
+
+        def fmt(task: Task) -> str:
+            rhs = self._rhs(task.rule)
+            a = ' '.join(rhs[:task.dot])
+            b = ' '.join(rhs[task.dot:])
+            return f'({self._lhs(task.rule)}->{a}.{b}, {week[task.start % 7]})'
+
+        print('Worklists:')
+        for j, tasks in enumerate(self.chart):
+            print(f'J{j}: [' + ', '.join(fmt(t) for t in tasks) + ']')
 
 
-def recognize(tokens):
-    worklists = [[Task(0, 0, 0)]]
-
-    def add_tasks(i, task):
-        if len(worklists) <= i:
-            worklists.append([])
-        if task not in worklists[i]:
-            worklists[i].append(task)
-
-    token_counter = 0
-    while True:
-        token = next(tokens, None)
-
-        if token_counter == len(worklists):
-            return False
-
-        i = 0
-        while i < len(worklists[token_counter]):
-            task = worklists[token_counter][i]
-            next_symbol = task.next_symbol()
-
-            # Completion
-            if next_symbol is None:
-                for prev in worklists[task.start]:
-                    if prev.next_symbol() == grammar[task.rule][0]:
-                        add_tasks(token_counter, Task(prev.rule, prev.dot + 1, prev.start))
-
-            # Scanning
-            elif next_symbol in terminals:
-                if token is not None and next_symbol == token:
-                    add_tasks(token_counter + 1, Task(task.rule, task.dot + 1, task.start))
-
-            # Prediction
-            else:
-                for idx, (lhs, rhs) in enumerate(grammar):
-                    if lhs == next_symbol:
-                        add_tasks(token_counter, Task(idx, 0, token_counter))
-
-            i += 1
-
-        if token is None:
-            break
-        token_counter += 1
-
-    # Check for finished grammar
-    cur = [task for task in worklists[-1] if task == Task(0, len(grammar[0][1]), 0)]
-    print("Worklists:")
-    for i, tasks in enumerate(worklists):
-        print(f"J{i}: {tasks}")
-    return bool(cur)
-
-
-print(recognize(iter(['1', '+', '1'])))
+if __name__ == '__main__':
+    parser = EarleyParser(debug=True)
+    print(parser.recognize(['1', '+', '1']))
